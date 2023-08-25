c01a88ed5d3460a89476d83823b9290682314f8e
Gourav Hammad <gourav.hammad@bfhl.in>
2023-08-25 19:33:40 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+
+// Styles
 import "../src/dist/styles.css";
+
 import About from "./Pages/About";
 import Home from "./Pages/Home";
 import Navbar from "../src/components/Navbar";
-import { Route, Routes } from "react-router-dom";
+
 import Models from "./Pages/Models";
 import TestimonialsPage from "./Pages/TestimonialsPage";
 import Team from "./Pages/Team";
